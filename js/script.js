bb2d44f6dd08c90992c629a1aced66d6bdafe0f4
Jochem Vogel <voge0031@hz.nl>
2020-04-06 10:48:47 +0200

    Update outdated elements of the code/delete unnecessary code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+
+let rgbDisplay = document.getElementById('rgb-display');
+let statusDisplay = document.getElementById('status-message');
+let scoreDisplay = document.getElementById('score');
+let resetButton = document.getElementById('reset');
+
+let h1 = document.querySelector('h1');
+
+let levelButtons = document.querySelectorAll('.level');
+let squares = document.querySelectorAll('.square');
+let squaresDisplay = document.querySelectorAll('#squares');
+
+
 let colors = [];
 let numSquares = 6;
 let pickedColor;
@@ -5,21 +18,9 @@ let score = 0;
 let guesses = 0;
 let oneGuess = true;
 
-
-let rgbDisplay = document.getElementById("rgb-display");
-let h1 = document.querySelector("h1");
-let levelButtons = document.querySelectorAll(".level");
-let statusDisplay = document.querySelector("#status-message");
-let scoreDisplay = document.querySelector("#score");
-let squares = document.querySelectorAll(".square");
-let squaresDisplay = document.querySelectorAll("#squares");
-let resetButton = document.querySelector("#reset");
-
 init();
 
-/*
-This function will start the application
-*/
+// Start app
 function init() {
     setupLevelButtons();
     setupSquares();
@@ -27,168 +28,132 @@ function init() {
     updateScore();
 }
 
+// Update score
 function updateScore() {
-    scoreDisplay.textContent = "Score: " + score;
+    scoreDisplay.textContent = 'Score: ' + score;
 }
 
-/*
-This function will make all the level buttons work
-*/
+// Make level buttons work
 function setupLevelButtons() {
-    for(var i = 0; i < levelButtons.length; i++) {
-        levelButtons[i].addEventListener("click", function() {
-            levelButtons[0].classList.remove("selected");
-            levelButtons[1].classList.remove("selected");
-            levelButtons[2].classList.remove("selected");
-            levelButtons[3].classList.remove("selected");
-            this.classList.add("selected");
-            if(this.textContent === "Easy") {
+    levelButtons.forEach((item) => {
+        item.addEventListener('click', function() {
+            levelButtons[0].classList.remove('selected');
+            levelButtons[1].classList.remove('selected');
+            levelButtons[2].classList.remove('selected');
+            levelButtons[3].classList.remove('selected');
+            this.classList.add('selected');
+            if(this.textContent === 'Easy') {
                 numSquares = 3;
-            } else if (this.textContent === "Medium") {
+            } else if (this.textContent === 'Medium') {
                 numSquares = 6;
-            } else if (this.textContent === "Hard") {
+            } else if (this.textContent === 'Hard') {
                 numSquares = 9;
             } else {
                 numSquares = 30;
             }
             reset();
         });
-    }
+    });
 }
 
-/*
-This function need some refactoring
-*/
-
+// Set up the squares
 function setupSquares() {
-    for(let i = 0; i < squares.length; i++) {
-        squares[i].addEventListener("click", function() {
+    squares.forEach((square, index) => {
+        square.addEventListener('click', function() {
             let clickedColor = this.style.backgroundColor;
-            if(clickedColor === pickedColor) {
-                if(levelButtons[3].classList.contains("selected")) {
-                    if(guesses === 1) {
-                        statusDisplay.textContent = "OK, you're a designerd";
-                        resetButton.textContent = "Play Again?";
+
+            if(clickedColor == pickedColor) {
+
+                // Impossible level
+                if(levelButtons[3].classList.contains('selected')) {
+
+                    // In one guess (not working yet)
+                    if(guesses === 0) {
+                        statusDisplay.textContent = 'OK, you\'re a designerd';
+                        resetButton.textContent = 'Play Again?';
                         score++;
                         changeColors(clickedColor);
                         h1.style.backgroundColor = clickedColor;
+                    // In more than one guess
                     } else {
-                        statusDisplay.textContent = "Correct!";
-                        resetButton.textContent = "Play Again?";
+                        statusDisplay.textContent = 'Correct!';
+                        resetButton.textContent = 'Play Again?';
                         score++;
                         changeColors(clickedColor);
                         h1.style.backgroundColor = clickedColor;
                     }
+
+                // Easy, medium and hard level
                 } else 
-                    statusDisplay.textContent = "Correct!";
-                    resetButton.textContent = "Play Again?";
+                    statusDisplay.textContent = 'Correct!';
+                    resetButton.textContent = 'Play Again?';
                     score++;
                     changeColors(clickedColor);
                     h1.style.backgroundColor = clickedColor;
+
+            // If clickedColor !== pickedColor
             } else {
-                this.style.backgroundColor = "#232323";
-                statusDisplay.textContent = "Try Again";
+                this.style.backgroundColor = '#232323';
+                statusDisplay.textContent = 'Try Again';
                 score = 0;
                 oneGuess = false;
-            }
-        });
-    }
+            } 
+        })
+    });
 }
 
-/*
-This function will pick a new color and will reset the score
-*/
+// Pick new color and reset score
 function reset() {
     colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     rgbDisplay.textContent = pickedColor;
-    resetButton.textContent = "Reset Colors";
-    statusDisplay.textContent = "";
+    resetButton.textContent = 'Reset Colors';
+    statusDisplay.textContent = '';
     score = 0;
     oneGuess = true;
-    for(var i = 0; i<squares.length; i++) {
-        if(colors[i]) {
-            squares[i].style.display = "block";
-            squares[i].style.backgroundColor = colors[i];
-        } else {
-            squares[i].style.display = "none";
-        }
-    }
-    h1.style.background = "#309D00";
-}
 
-resetButton.addEventListener("click", function() {
-    reset();
+    squares.forEach((square, index) => {
+        if(colors[index]) {
+            square.style.display = 'block';
+            square.style.backgroundColor = colors[index];
+        } else {
+            square.style.display = 'none';
+        }
+    });
+
+    h1.style.background = '#309D00';
+    }
+    
+    resetButton.addEventListener('click', function() {
+        reset();
 });
 
-
-/*
-This function below will change all the squares
-to the same background color.
-*/
+// Change all squares to same background if answer is correct
 function changeColors(color) {
-    for(var i=0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = color;
-    }
+    squares.forEach(item => item.style.backgroundColor = color);
 }
 
-/*
-This function below will pick a random color.
-This random color will be the correct color during the game.
-*/
+// Pick a random color and this will be the correct color during the game
 function pickColor() {
     let random = Math.floor(Math.random() * colors.length);
     return colors[random];
 }
 
-/*
-This function will push random numbers to the array
-*/
+// Push random numbers to array
 function generateRandomColors(num) {
     let arr = []
     for(var i = 0; i < num; i++) {
         arr.push(randomColor());
     }
+    
     return arr;
+
 }
 
-/* 
-This function will create a random RGB color
-*/
+// Create random rgb color
 function randomColor() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     return "rgb(" + r + ", " + g + ", " + b + ")";
 }
-
-
-/*
-LATER:
-
----------------------
-
-Scores:
-- LEVEL: [numSquares] points (wrong -[numSquares / 3])
-
-- Easy: right +3 points | wrong -1 points
-- Medium: right +6 points | wrong -2 points
-- Hard: right +9 points | wrong -3 points
-- Impossible: right 30 points | wrong -10 points
-
-
---------------------
-
-function setUpSquares() {
-    for(let i = 0; i < squares.length; i++) {
-        squares[i].addEventListener("click", function() {
-            let clickedColor = this.style.backgroundColor;
-            if(clickedColor === pickedColor) {
-                score = score + numSquares;
-            } else {
-                score = score - (numSquares / 3);
-            }
-        }
-    }
-}
-*/
